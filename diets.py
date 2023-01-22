a941ba09de1b0e3bc875b1dbfacb932e1fd664ee
LethalSainth <ikechukwuedwin01@gmail.com>
2023-01-22 21:24:41 +0100

    add diets

---
+++ diets.py
@@ -0,0 +1,8 @@
+days_of_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday']
+breakfast = ['Bread', 'Egg', 'Oat']
+lunch =  ['Rice', 'Yam', 'Semo'] 
+dinner = ['Fish stew', 'Beans', 'Pepperoni']
+
+diet= [meal for meal in breakfast]
+print(diet)
+# day for day in days_of_week
