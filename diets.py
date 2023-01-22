8660d6cd8dcb2cd1b9c033582ab113d090803b0a
LethalSainth <ikechukwuedwin01@gmail.com>
2023-01-22 22:29:50 +0100

    testing loops

--- diets.py
+++ diets.py
@@ -3,6 +3,19 @@ breakfast = ['Bread', 'Egg', 'Oat']
 lunch =  ['Rice', 'Yam', 'Semo'] 
 dinner = ['Fish stew', 'Beans', 'Pepperoni']
 
-diet= [meal for meal in breakfast]
-print(diet)
-# day for day in days_of_week
+for i in range(0,7):
+    print(i)
+
+# diet= [[day,meal for day,meal in days_of_week, breakfast] for i in range(0,7)]
+# print(diet)
+
+
+
+# Day         breakfast    lunch       dinner
+# sunday  - 
+# monday
+# tuesday
+# wednesday
+# thursday
+# friday
+# saturday
