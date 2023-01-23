fbf29bce43e60d543d7002ee09fe17e1bac7dd9e
LethalSainth <ikechukwuedwin01@gmail.com>
2023-01-23 21:33:20 +0100

    looping

--- diets.py
+++ diets.py
@@ -1,10 +1,16 @@
 days_of_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday']
-breakfast = ['Bread', 'Egg', 'Oat']
+breakfast = ['Bread', 'Egg', 'Oat', 'Junk', 'Junk1', 'Junk2', 'Junk3' ]
 lunch =  ['Rice', 'Yam', 'Semo'] 
 dinner = ['Fish stew', 'Beans', 'Pepperoni']
 
-for i in range(0,7):
-    print(i)
+diet = []
+
+for day in days_of_week:
+    for fast in breakfast:
+        diet.append(day)
+        diet.append(fast)
+
+print(diet)
 
 # diet= [[day,meal for day,meal in days_of_week, breakfast] for i in range(0,7)]
 # print(diet)
