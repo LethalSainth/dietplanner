ac884c6f57909bb998f5c38bd6264ae15581ea2c
LethalSainth <ikechukwuedwin01@gmail.com>
2023-01-24 16:06:00 +0100

    added working code

--- diets.py
+++ diets.py
@@ -1,27 +1,31 @@
 days_of_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday']
+
+#Underweight diet
+under_diet = []
 breakfast = ['Bread', 'Egg', 'Oat', 'Junk', 'Junk1', 'Junk2', 'Junk3' ]
 lunch =  ['Rice', 'Yam', 'Semo'] 
 dinner = ['Fish stew', 'Beans', 'Pepperoni']
 
-diet = []
+#Normal diet
+under_diet = []
+breakfast = ['Bread', 'Egg', 'Oat', 'Junk', 'Junk1', 'Junk2', 'Junk3' ]
+lunch =  ['Rice', 'Yam', 'Semo'] 
+dinner = ['Fish stew', 'Beans', 'Pepperoni']
 
-for day in days_of_week:
-    for fast in breakfast:
-        diet.append(day)
-        diet.append(fast)
+#Overweight diet
+under_diet = []
+breakfast = ['Bread', 'Egg', 'Oat', 'Junk', 'Junk1', 'Junk2', 'Junk3' ]
+lunch =  ['Rice', 'Yam', 'Semo'] 
+dinner = ['Fish stew', 'Beans', 'Pepperoni']
+
+#Obese diet
+under_diet = []
+breakfast = ['Bread', 'Egg', 'Oat', 'Junk', 'Junk1', 'Junk2', 'Junk3' ]
+lunch =  ['Rice', 'Yam', 'Semo'] 
+dinner = ['Fish stew', 'Beans', 'Pepperoni']
+
+for day,fast,lun,dine in zip(days_of_week,breakfast,lunch,dinner):
+    diet.append([day,fast,lun,dine])
 
 print(diet)
 
-# diet= [[day,meal for day,meal in days_of_week, breakfast] for i in range(0,7)]
-# print(diet)
-
-
-
-# Day         breakfast    lunch       dinner
-# sunday  - 
-# monday
-# tuesday
-# wednesday
-# thursday
-# friday
-# saturday
